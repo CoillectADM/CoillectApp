f7ce7e3726e2661c3110fe148e551c09afa1a523
Coillect.Dev_Allan <coillect.app@gmail.com>
2025-11-07 04:35:37 -0300

    add swagger sem DTOdecorators

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Prefixo global de rotas
   app.setGlobalPrefix('api');
 
+  // Pipes globais de validação
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -16,9 +18,23 @@ async function bootstrap() {
     }),
   );
 
+  // CORS liberado
   app.enableCors({
     origin: true,
   });
+
+  // 📘 Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Coillect API')
+    .setDescription('Documentação interativa da API Coillect')
+    .setVersion('1.0')
+    .addBearerAuth() // adiciona suporte JWT (Authorization: Bearer)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  // Inicializa o servidor
   await app.listen(3000);
 }
 bootstrap();
